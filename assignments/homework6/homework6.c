89399990914e3d1b7c6cdbabdccf37719cefae91
MintPaw <jerusanders@gmail.com>
2015-04-14 17:40:32 -0600

    Added some homework6 files

---
+++ assignments/homework6/homework6.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int array1[3][3] = {{1, 2, 3},
+                      {4, 5, 6},
+                      {7, 8, 9}};
+
+    int array2[3][3] = {{1, 2, 3},
+                      {4, 5, 6},
+                      {7, 8, 9}};
+
+    int i;
+    int j;
+    int width = 3;
+    int height = 3;
+    int line1[]
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            int h;
+            int line[]
+            for (h = 0; h < width; h++)
+            {
+                line1[h] = array1[i];
+            }
+            
+        }
+    }
+
+    return 0;
+}
+
+
+
